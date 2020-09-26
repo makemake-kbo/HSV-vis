a1a3381181fdbf95586677c1f696cfd46d2e2a45
vgostovic <vukasin@gostovic.de>
2020-09-26 16:50:16 +0200

    Forgot to stage about window :P

---
+++ AboutWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HSVvis
+{
+	public partial class AboutWindow : Gtk.Window
+	{
+		public AboutWindow () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
